227a1c1b6d977a8c44fe4524fdb555117a800c67
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-11-18 02:11:57 +0300

    TMP

--- BoardConfigAnx.mk
+++ BoardConfigAnx.mk
@@ -1,3 +1,3 @@
 # Sepolicy
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    vendor/xiaomi/miuicamera/sepolicy/private
+#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+#    vendor/xiaomi/miuicamera/sepolicy/private
